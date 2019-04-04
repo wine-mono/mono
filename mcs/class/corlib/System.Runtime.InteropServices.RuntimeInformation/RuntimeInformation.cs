653b0d264422ed4ef82a67d5affc508dcf6d46e8
Vincent Povirk <vincent@codeweavers.com>
2019-04-04 13:14:27 -0500

    Remove Mono.Runtime type.
    For wine bug 37998.

--- mcs/class/corlib/System.Runtime.InteropServices.RuntimeInformation/RuntimeInformation.cs
+++ mcs/class/corlib/System.Runtime.InteropServices.RuntimeInformation/RuntimeInformation.cs
@@ -45,7 +45,7 @@ namespace System.Runtime.InteropServices
 
 		public static string FrameworkDescription {
 			get {
-				return "Mono " + Mono.Runtime.GetDisplayName ();
+				return ".NET Framework 4.7.3362.0";
 			}
 		}
 
