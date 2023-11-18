2542811634f28523dac3b35d59a71befcb887805
Esme Povirk <esme@codeweavers.com>
2023-11-18 18:08:45 +0000

    mini-amd64: Use TIB.self field to get last error.
    Direct gs access of last error field doesn't work on macOS.

--- mono/mini/mini-amd64.c
+++ mono/mini/mini-amd64.c
@@ -4392,7 +4392,8 @@ emit_get_last_error (guint8* code, int dreg)
 {
 	/* Threads last error value is located in TEB_LAST_ERROR_OFFSET. */
 	x86_prefix (code, X86_GS_PREFIX);
-	amd64_mov_reg_mem (code, dreg, TEB_LAST_ERROR_OFFSET, sizeof (guint32));
+	amd64_mov_reg_mem (code, dreg, 0x30, 8);
+	amd64_mov_reg_membase (code, dreg, dreg, TEB_LAST_ERROR_OFFSET, sizeof (guint32));
 	return code;
 }
 
