fbd54c587f7b7e38dbc610239fbb22eecb92843d
Esme Povirk <esme@codeweavers.com>
2024-04-20 17:35:48 +0000

    Gitlab CI: Print timestamps during build.

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -4,8 +4,8 @@ set -e
 
 ./autogen.sh
 
-make -j$(nproc) all
+make -j$(nproc) all | ts '[%H:%M:%S]'
 
-make test
+make test | ts '[%H:%M:%S]'
 
-make TEST_BUNDLE_PATH=$PWD/test-bundle test-bundle
+make TEST_BUNDLE_PATH=$PWD/test-bundle test-bundle | ts '[%H:%M:%S]'
