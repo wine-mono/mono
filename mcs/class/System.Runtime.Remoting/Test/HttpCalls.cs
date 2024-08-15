d92ff2e7c515d5c59b4eabf929bef9a4c13ac517
Bernhard Kölbl <besentv@gmail.com>
2024-08-15 22:35:42 +0200

    [System.Runtime.Remoting] Add custom sink tests.

--- mcs/class/System.Runtime.Remoting/Test/HttpCalls.cs
+++ mcs/class/System.Runtime.Remoting/Test/HttpCalls.cs
@@ -8,6 +8,8 @@
 
 using System;
 using System.Collections;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Http;
@@ -61,7 +63,26 @@ namespace MonoTests.Remoting
 			return new HttpChannelManager ();
 		}
 	}
+
+	// Needs separate tests.
+	[TestFixture]
+	public class HttpCustomSyncCallTest : SyncCallTest
+	{
+		public override ChannelManager CreateChannelManager ()
+		{
+			return new HttpCustomChannelManager ();
+		}
+	}
 */
+
+	[TestFixture]
+	public class HttpCustomAsyncCallTest : AsyncCallTest
+	{
+		public override ChannelManager CreateChannelManager ()
+		{
+			return new HttpCustomChannelManager ();
+		}
+	}
 
 	[Serializable]
 	public class HttpChannelManager : ChannelManager
@@ -93,6 +114,37 @@ namespace MonoTests.Remoting
 		public override IChannelReceiver CreateServerChannel ()
 		{
 			return new HttpChannel (0);
+		}
+	}
+
+	[Serializable]
+	public class HttpCustomChannelManager : ChannelManager
+	{
+		public override IChannelSender CreateClientChannel ()
+		{
+			Hashtable channelOptions = new Hashtable ();
+			channelOptions ["timeout"] = 5000; // 5s
+			channelOptions ["name"] = "http channel with custom sink";
+
+			Hashtable providerOptions = new Hashtable ();
+			providerOptions["customSinkType"] = "MonoTests.Remoting.SimpleCustomClientSink";
+
+			BinaryClientFormatterSinkProvider sinkProvider = new BinaryClientFormatterSinkProvider ();
+			sinkProvider.Next = new CustomClientSinkProvider (providerOptions, new Hashtable ());
+			return new HttpClientChannel (channelOptions,  sinkProvider);
+		}
+
+		public override IChannelReceiver CreateServerChannel ()
+		{
+			Hashtable channelOptions = new Hashtable ();
+			channelOptions ["port"] = "0";
+
+			Hashtable providerOptions = new Hashtable ();
+			providerOptions["customSinkType"] = "MonoTests.Remoting.SimpleCustomServerSink";
+
+			BinaryServerFormatterSinkProvider sinkProvider = new BinaryServerFormatterSinkProvider ();
+			sinkProvider.Next = new CustomServerSinkProvider (providerOptions, new Hashtable ());
+			return new HttpChannel (channelOptions, (IClientChannelSinkProvider)null, (IServerChannelSinkProvider)sinkProvider);
 		}
 	}
 }
