cdd9f0253b18d44b25c5222c767e760b63c4caf6
Esme Povirk <esme@codeweavers.com>
2025-03-21 16:25:35 +0000

    CI scripts: Add linux x86 docker environment.

---
+++ scripts/ci/podman-x86-exec.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+SRCDIR=$(realpath "$(dirname "$(realpath "$0")")/../..")
+
+podman image build -f ${SRCDIR}/scripts/ci/linux-x86.docker -t mono-linux-x86 || exit 1
+
+mkdir -p "${SRCDIR}/.podman-home" || exit 1
+
+exec podman run -u root -t --init -a stdin -a stdout -a stderr --volume "${SRCDIR}:/var/hostdir:Z" --volume "${SRCDIR}/.podman-home:/root:Z" -w /var/hostdir --read-only mono-linux-x86 "$@"
+
