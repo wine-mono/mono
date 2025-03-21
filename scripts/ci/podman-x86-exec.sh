ae6c74419a000a215b3296cf4501a5cfabdc44fe
Esme Povirk <esme@codeweavers.com>
2025-03-21 16:45:54 +0000

    CI scripts: Specify platform when invoking podman.

--- scripts/ci/podman-x86-exec.sh
+++ scripts/ci/podman-x86-exec.sh
@@ -6,5 +6,5 @@ podman image build -f ${SRCDIR}/scripts/ci/linux-x86.docker -t mono-linux-x86 ||
 
 mkdir -p "${SRCDIR}/.podman-home" || exit 1
 
-exec podman run -u root -t --init -a stdin -a stdout -a stderr --volume "${SRCDIR}:/var/hostdir:Z" --volume "${SRCDIR}/.podman-home:/root:Z" -w /var/hostdir --read-only mono-linux-x86 "$@"
+exec podman run --platform linux/386 -u root -t --init -a stdin -a stdout -a stderr --volume "${SRCDIR}:/var/hostdir:Z" --volume "${SRCDIR}/.podman-home:/root:Z" -w /var/hostdir --read-only mono-linux-x86 "$@"
 
