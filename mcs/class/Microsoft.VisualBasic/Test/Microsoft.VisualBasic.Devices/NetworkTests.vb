a20009e08ab6236b7e2c946d114555b76868f52e
Esme Povirk <esme@codeweavers.com>
2025-06-19 19:24:55 +0000

    Microsoft.VisualBasic/Test: Don't try to ping nonexistent host.
    Ping throws an exception (including on Windows) if the host fails
    to resolve.

--- mcs/class/Microsoft.VisualBasic/Test/Microsoft.VisualBasic.Devices/NetworkTests.vb
+++ mcs/class/Microsoft.VisualBasic/Test/Microsoft.VisualBasic.Devices/NetworkTests.vb
@@ -85,17 +85,11 @@ Namespace Devices
             End If
 
             Dim realHost As String = "www.google.com"
-            Dim imaginaryHost As String = "www.changemeifiexist.com"
 
             Assert.AreEqual(True, nw.Ping(realHost), testname & "-12")
             Assert.AreEqual(True, nw.Ping(realHost, 10000), testname & "-13")
             Assert.AreEqual(True, nw.Ping(New Uri("http://" & realHost)), testname & "-14")
             Assert.AreEqual(True, nw.Ping(New Uri("http://" & realHost), 10000), testname & "-15")
-
-            Assert.AreEqual(False, nw.Ping(imaginaryHost), testname & "-22")
-            Assert.AreEqual(False, nw.Ping(imaginaryHost, 10000), testname & "-23")
-            Assert.AreEqual(False, nw.Ping(New Uri("http://" & imaginaryHost)), testname & "-24")
-            Assert.AreEqual(False, nw.Ping(New Uri("http://" & imaginaryHost), 10000), testname & "-25")
 
         End Sub
 
