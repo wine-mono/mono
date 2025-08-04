f6b7e35092a82a8cd310d3054c50d76c0420a686
Esme Povirk <esme@codeweavers.com>
2025-08-04 18:40:02 +0000

    Microsoft.VisualBasic: Import Microsoft.VisualBasic.Forms from .NET.

---
+++ mcs/class/Microsoft.VisualBasic/CoreWinforms/Microsoft/VisualBasic/CompilerServices/SR.vb
@@ -0,0 +1,12 @@
+
+Namespace Microsoft.VisualBasic.CompilerServices
+	Friend Partial Class SR
+		Public Shared ReadOnly AppModel_SplashAndMainFormTheSame As String = "AppModel_SplashAndMainFormTheSame"
+		Public Shared ReadOnly Argument_InvalidNullValue1 As String = "Argument_InvalidNullValue1"
+		Public Shared ReadOnly Argument_InvalidValue1 As String = "Argument_InvalidValue1"
+		Public Shared ReadOnly Argument_InvalidValueType2 As String = "Argument_InvalidValueType2"
+		Public Shared ReadOnly General_ArgumentNullException As String = "General_ArgumentNullException"
+		Public Shared ReadOnly General_PropertyNothing As String = "General_PropertyNothing"
+		Public Shared ReadOnly ProcessNotFound As String = "ProcessNotFound"
+	End Class
+End Namespace
