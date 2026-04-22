wine-mono/mono

commit 5f4275f5d2a419b5f7f7ad4efa4776fe7dd330e3

utils: Only use LOAD_WITH_ALTERED_SEARCH_PATH with absolute paths.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=56301

mono/utils/mono-dl.c
@@ -183,7 +183,7 @@ MonoDl*
 mono_dl_open (const char *name, int flags, char **error_msg)
 {
 #ifdef HOST_WIN32
-	const int native_flags = LOAD_WITH_ALTERED_SEARCH_PATH;
+	const int native_flags = g_path_is_absolute (name) ? LOAD_WITH_ALTERED_SEARCH_PATH : 0;
 #else
 	const int native_flags = 0;
 #endif

commit ab507d3cb084be9f0474bad660220326f0caecae

utils: Fix assert caused by 5f4275f5.

mono/utils/mono-dl.c
@@ -183,7 +183,7 @@ MonoDl*
 mono_dl_open (const char *name, int flags, char **error_msg)
 {
 #ifdef HOST_WIN32
-	const int native_flags = g_path_is_absolute (name) ? LOAD_WITH_ALTERED_SEARCH_PATH : 0;
+	const int native_flags = (name && g_path_is_absolute (name)) ? LOAD_WITH_ALTERED_SEARCH_PATH : 0;
 #else
 	const int native_flags = 0;
 #endif